a0837c1a6b0bf690864d66efd4f7d871f5b7bba3
seajihey <okcoco03@naver.com>
2026-02-18 22:57:06 +0900

    refactor: initial-folder-tree-refacotr

---
+++ common/src/test/java/msa/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
