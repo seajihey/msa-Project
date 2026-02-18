172e2c38a5ab919a6522f271821de2d0d2fecdb8
seajihey <okcoco03@naver.com>
2026-02-18 21:47:20 +0900

    feat: initial-folder-tree

---
+++ ledger-service/ledger-service/src/test/java/msa/ledgerservice/LedgerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.ledgerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
